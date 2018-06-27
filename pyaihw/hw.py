63febc17c991bd54714725bbbb29c3f7948a18dc
Ananth Iyer <Ananth.Iyer@genesys.com>
2018-06-27 11:39:47 -0400

    modified source to use datafiles

--- pyaihw/hw.py
+++ pyaihw/hw.py
@@ -1,2 +1,7 @@
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 def say():
-    print('Hello World -ai')
+    with open(os.path.join(dir_path, 'data', 'name.txt')) as fp:
+        name = fp.read()
+    print('Hello {}'.format(name))
--- setup.py
+++ setup.py
@@ -2,8 +2,16 @@ from distutils.core import setup
 
 setup(
     name='PyAIHW',
-    version='0.1dev',
-    packages=['pyaihw',],
+    version='0.2dev',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
+    packages = ['pyaihw',],  # include all packages under src
+
+    package_data = {
+        # If any package contains *.txt files, include them:
+        '': ['*.py'],
+        # And include any *.dat files found in the 'data' subdirectory
+        # of the 'mypkg' package, also:
+        'pyaihw': ['data/*.txt'],
+    }
 )
