9b60ea594027fe9dbf2c46f3eead37f934c8fad2
Ananth Iyer <Ananth.Iyer@genesys.com>
2018-06-27 22:02:30 -0400

    add dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyAIHW',
-    version='0.3',
+    version='0.4',
     license='',
     long_description=open('README.txt').read(),
     packages = ['pyaihw',],
